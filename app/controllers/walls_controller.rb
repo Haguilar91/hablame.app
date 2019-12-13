48801b4d24042dfbbec31186ba40bd8ca2c3eb98
Hector Aguilar <haguilar@haguilar.local>
2019-12-13 16:28:47 -0600

    Tratando de hacer que regrese a conversacion ya hecha en pantalla de seleccion

--- app/controllers/walls_controller.rb
+++ app/controllers/walls_controller.rb
@@ -1,7 +1,13 @@
 class WallsController < ApplicationController
   before_action :authenticate_user!
   def show_all_doctors
+    if (current_user)
+    @conversation = current_user.mailbox.conversations.last.find(current_user.id)
+    @current_doctor = @conversation.participants.find{|p| p != current_user}
+    end
+   
     @doctors = Doctors.all
+    
   end
 
   def doctor_index; end
