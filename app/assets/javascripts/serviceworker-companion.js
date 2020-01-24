f0c1daa087540d42352de822ba38112f5df9f355
Hector Aguilar <haguilar91@gmail.com>
2020-01-24 00:06:37 -0600

    PWA

---
+++ app/assets/javascripts/serviceworker-companion.js
@@ -0,0 +1,17 @@
+// Este es el serviceworker con Cache-first Network.
+
+// Comprueba la compatibilidad del navegador en el que estamos ejecutando esto.
+if ("serviceWorker" in navigator) {
+    if (navigator.serviceWorker.controller) {
+      console.log("[PWA Builder] active service worker found, no need to register");
+    } else {
+      // Registra el serviceworker
+      navigator.serviceWorker
+        .register("/serviceworker.js", {
+          scope: "./"
+        })
+        .then(function (reg) {
+          console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
+        });
+    }
+  }
