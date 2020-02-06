7f726b8f989701ecb463623f2abf15d59877c088
Hector Aguilar <haguilar91@gmail.com>
2020-02-06 00:05:37 -0600

    Bug Fix still issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-  prepend_before_action :set_locale
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  
+    protect_from_forgery with: :exception
+  prepend_before_action :set_locale
+
   before_action :masquerade_user!
 
   protected
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
+	skip_before_action :verify_authenticity_token
   def index
+
     if (!current_user)
       render file: 'home/index.html.erb'
     else
