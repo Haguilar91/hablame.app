263133cae896c70c919ccfc278cd23e891fdd777
Hector Aguilar <haguilar91@gmail.com>
2020-03-25 00:02:07 -0600

    Undoing

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -17,7 +17,3 @@ if ("serviceWorker" in navigator) {
     }
   }
 
-  import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
-
-  const el = document.createElement('pwa-update');
-  document.body.appendChild(el);
