b8f1ee401177d6e9c04afa49242c1be35187aa4e
Cloud User <centos@ip-172-31-10-208.us-east-2.compute.internal>
2020-03-25 00:39:11 -0600

    PWA Cache fixed

--- app/assets/javascripts/serviceworker-companion.js
+++ app/assets/javascripts/serviceworker-companion.js
@@ -1,19 +1,28 @@
 // Este es el serviceworker con Cache-first Network.
 
+
+
+// Este es el serviceworker con Cache-first Network.
+
 // Comprueba la compatibilidad del navegador en el que estamos ejecutando esto.
 
-if ("serviceWorker" in navigator) {
-    if (navigator.serviceWorker.controller) {
-      console.log("[PWA Builder] active service worker found, no need to register");
-    } else {
-      // Registra el serviceworker
-      navigator.serviceWorker
-        .register("/serviceworker.js", {
-          scope: "./"
-        })
-        .then(function (reg) {
-          console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
-        });
-    }
-  }
+//if ("serviceWorker" in navigator) {
+ //   if (navigator.serviceWorker.controller) {
+ //     console.log("[PWA Builder] active service worker found, no need to register");
+ //   } else {
+ //     // Registra el serviceworker
+ //     navigator.serviceWorker
+  //      .register("/serviceworker.js", {
+   //       scope: "./"
+   //     })
+   //     .then(function (reg) {
+   //       console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
+   //     });
+  //  }
+ // }
+
+import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
 
